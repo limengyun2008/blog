4fe818fee493e3ddee8b8642beb0f3e02ee978ce
Li Mengyun <limy@wandoujia.com>
2015-01-15 16:53:01 +0800

    mod url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.limengyun.com'
+SITEURL = 'http://blog.limengyun.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
