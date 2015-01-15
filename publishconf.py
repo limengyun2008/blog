0728ecf5ec892dcfbab35e261bd2ddf53a6ddd82
Li Mengyun <limy@wandoujia.com>
2015-01-15 16:20:09 +0800

    change sitename

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.limengyun.com'
+SITEURL = 'http://www.limengyun.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
